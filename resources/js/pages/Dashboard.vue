ba3cd27f509f62b97549b1689e83004c376d106c
Lawrence <lawrencelimjl@gmail.com>
2026-02-01 02:30:33 +0800

    optimize: add max width for timecard in dashboard

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -33,7 +33,7 @@ const breadcrumbs: BreadcrumbItem[] = [
         <div
             class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4"
         >
-            <div class="grid auto-rows-min gap-4 md:grid-cols-3">
+            <div class="grid auto-rows-min gap-4 md:grid-cols-3 max-w-4xl">
                 <!-- Clock Widget (only show if user has stores assigned) -->
                 <div v-if="stores && stores.length > 0" class="md:col-span-2">
                     <ClockWidget
