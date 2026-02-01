14f83177946b484ab5084806f5c9881f4028a794
Lawrence <lawrencelimjl@gmail.com>
2026-02-01 13:23:15 +0800

    fix: wayfinder from routes generation

--- routes/api.php
+++ routes/api.php
@@ -25,10 +25,8 @@ Route::prefix('v1')->group(function () {
             ->name('api.employees.store');
         Route::get('/employees/{employee}', [UserController::class, 'show'])
             ->name('api.employees.show');
-        Route::put('/employees/{employee}', [UserController::class, 'update'])
+        Route::match(['put', 'patch'], '/employees/{employee}', [UserController::class, 'update'])
             ->name('api.employees.update');
-        Route::patch('/employees/{employee}', [UserController::class, 'update'])
-            ->name('api.employees.patch');
         Route::delete('/employees/{employee}', [UserController::class, 'destroy'])
             ->name('api.employees.destroy');
 
@@ -40,10 +38,8 @@ Route::prefix('v1')->group(function () {
             ->name('api.customers.store');
         Route::get('/customers/{customer}', [UserController::class, 'showCustomer'])
             ->name('api.customers.show');
-        Route::put('/customers/{customer}', [UserController::class, 'updateCustomer'])
+        Route::match(['put', 'patch'], '/customers/{customer}', [UserController::class, 'updateCustomer'])
             ->name('api.customers.update');
-        Route::patch('/customers/{customer}', [UserController::class, 'updateCustomer'])
-            ->name('api.customers.patch');
         Route::delete('/customers/{customer}', [UserController::class, 'destroyCustomer'])
             ->name('api.customers.destroy');
 
